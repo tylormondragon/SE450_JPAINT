60baafd65bd666c9eb8a836e070b8967d9b739f5
Tylor Mondragon <twaitsmo@alumni.nd.edu>
2019-07-31 22:25:49 -0500

    Copy and Paste

--- src/view/interfaces/PaintCanvasBase.java
+++
@@ -1,8 +0,0 @@
-package view.interfaces;
-
-import javax.swing.*;
-import java.awt.*;
-
-public abstract class PaintCanvasBase extends JComponent {
-    public abstract Graphics2D getGraphics2D();
-}
